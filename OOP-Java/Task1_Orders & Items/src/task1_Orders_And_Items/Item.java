bd1d8e5ab141d210f8fe77238a8ec71e9f483e31
Mohammad-Al-Turk <mohammad.al.turk.mt@gmail.com>
2024-12-09 08:39:18 +0200

    1st OOP-Java Task

---
+++ OOP-Java/Task1_Orders & Items/src/task1_Orders_And_Items/Item.java
@@ -0,0 +1,8 @@
+package task1_Orders_And_Items;
+
+public class Item {
+	String name;
+	double price;
+
+
+}
---
+++ OOP-Java/Task1_Orders & Items/src/task1_Orders_And_Items/Order.java
@@ -0,0 +1,12 @@
+package task1_Orders_And_Items;
+
+import java.util.ArrayList;
+
+public class Order {
+	String name;
+	double total;
+	boolean ready;
+	ArrayList<Item> items = new ArrayList<Item>();
+	
+
+}
