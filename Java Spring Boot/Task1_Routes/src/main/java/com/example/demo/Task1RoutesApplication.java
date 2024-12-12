1563958223954587f2433f35a03c40fe1cfaec96
Mohammad-Al-Turk <mohammad.al.turk.mt@gmail.com>
2024-12-12 15:28:41 +0200

    1st Spring-Boot Task

---
+++ Java Spring Boot/Task1_Routes/src/main/java/com/example/demo/Task1RoutesApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Task1RoutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task1RoutesApplication.class, args);
+    }
+    
+}
